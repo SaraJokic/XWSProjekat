5e6b54f9a2fd17badbbe016cde55d10ebc32b487
tkrgovic <tasakrgovic@gmail.com>
2023-04-01 18:22:28 +0200

    Added dialog for buying tickets

--- FlightsFrontend/FlightsFrontend/src/app/components/buy-ticket-dialog/buy-ticket-dialog.component.ts
+++ FlightsFrontend/FlightsFrontend/src/app/components/buy-ticket-dialog/buy-ticket-dialog.component.ts
@@ -1,5 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Flights } from 'src/models/flight.model';
+import { Ticket } from 'src/models/ticket';
+import { TicketService } from 'src/services/ticket.service';
 
 @Component({
   selector: 'app-buy-ticket-dialog',
@@ -8,10 +12,37 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class BuyTicketDialogComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Flights, private ticketService: TicketService) { }
+  
+  numTickets: number = 1;
+  flight: Flights ={
+    id: this.data.id,
+    fromplace: this.data.fromplace,
+    toplace:this.data.toplace,
+    starttime: this.data.starttime ? new Date(this.data.starttime) : undefined,
+    endtime: this.data.endtime ? new Date(this.data.endtime) : undefined,
+    ticketprice: this.data.ticketprice,
+    numofseats:this.data.numofseats
+  };
   
   ngOnInit(): void {
     console.log(this.data)
+  }
+  buyTicket(){
+    const newTicket: Ticket = {
+      userid: "6426f65971b16d7d27fe5bb8",
+      flightid: this.data.id!,
+      expired: false,
+      quantity: this.numTickets,
+    };
+    this.ticketService.add(newTicket).subscribe(
+      (data) => {
+        alert("Success!");
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
   }
 
 }
--- FlightsFrontend/FlightsFrontend/src/services/dialog.service.ts
+++ FlightsFrontend/FlightsFrontend/src/services/dialog.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { BuyTicketDialogComponent } from 'src/app/components/buy-ticket-dialog/buy-ticket-dialog.component';
+import { Flights } from 'src/models/flight.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +10,15 @@ export class DialogService {
 
   constructor(private dialog: MatDialog) { }
 
-  openDialog(details: any): void {
+  openDialog(details: Flights): void {
     const dialogRef = this.dialog.open(BuyTicketDialogComponent, {
-      width: '400px',
+      width: '600px',
       height: '500px',
       data: details
     });
 
     dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
+      //console.log(`Dialog result: ${result}`);
     });
   }
 }
